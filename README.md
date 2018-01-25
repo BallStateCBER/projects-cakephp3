317b5cd8237f4840214ce0504df32279025f5f87
Graham Watson <graham@phantomwatson.com>
2018-01-25 15:29:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CBER Projects and Publications
+
+This is the codebase for https://projects.cberdata.org.
---
+++ tests/TestCase/Controller/PagesControllerTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         1.2.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Test\TestCase\Controller;
+
+use Cake\Core\Configure;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * PagesControllerTest class
+ */
+class PagesControllerTest extends IntegrationTestCase
+{
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->configRequest([
+            'environment' => ['HTTPS' => 'on']
+        ]);
+    }
+
+    /**
+     * testMultipleGet method
+     *
+     * @return void
+     */
+    public function testMultipleGet()
+    {
+        $this->get('/');
+        $this->assertResponseOk();
+        $this->get('/');
+        $this->assertResponseOk();
+    }
+
+    /**
+     * Test that missing template renders 404 page in production
+     *
+     * @return void
+     */
+    public function testMissingTemplate()
+    {
+        Configure::write('debug', false);
+        $this->get('/pages/not_existing');
+
+        $this->assertResponseError();
+        $this->assertResponseContains('Error');
+    }
+}
